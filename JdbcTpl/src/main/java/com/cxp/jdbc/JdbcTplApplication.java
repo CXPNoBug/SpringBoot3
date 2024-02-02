2998534e2915f75720b201a2efc072dfa8e05747
Jin Cheng <cxpnobug@163.com>
2024-02-03 00:35:08 +0800

    初始化

---
+++ JdbcTpl/src/main/java/com/cxp/jdbc/JdbcTplApplication.java
@@ -0,0 +1,13 @@
+package com.cxp.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcTplApplication.class, args);
+    }
+
+}
---
+++ JdbcTpl/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `article`
+VALUES ('1', '2101', 'SpringBoot 核心注解', '核心注解的主要作用', '00000008976', '2024-02-02 20:20:20');
+INSERT INTO `article`
+VALUES ('2', '356752', 'JVM 调优', 'HotSpot 虚拟机详解', '00000000026', '2024-02-02 20:40:30');
